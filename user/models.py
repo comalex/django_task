a1808ae71f385d59caf3f235cd150b18f3d1ec89
Alex Stupak <stupak.alexn@gmail.com>
2017-10-23 21:09:21 +0300

    added random_number filed to user model

--- user/models.py
+++ user/models.py
@@ -1,9 +1,17 @@
+import random
+
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
+def random_value():
+    return random.randint(1, 100)
+
+
 @python_2_unicode_compatible
 class User(AbstractUser):
     birth_day = models.DateField(_("Birth day"), null=True, blank=True)
+    random_number = models.PositiveIntegerField(default=random_value)
