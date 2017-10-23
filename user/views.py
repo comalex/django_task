71c60d9907091f57bf783cbb1f43dadf21c1c3f5
Alex Stupak <stupak.alexn@gmail.com>
2017-10-23 21:30:50 +0300

    user list

--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import User
 
-# Create your views here.
+
+class UserListView(ListView):
+    model = User
