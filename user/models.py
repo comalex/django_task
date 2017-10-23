602112f5a92334f2a68fcdf6cda64ff5f5a12d06
Alex Stupak <stupak.alexn@gmail.com>
2017-10-23 21:00:20 +0300

    added birt_day field to user model

--- user/models.py
+++ user/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 
 @python_2_unicode_compatible
 class User(AbstractUser):
-    pass
+    birth_day = models.DateField(_("Birth day"), null=True, blank=True)
